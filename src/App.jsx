4007e0c25a111e2ce2a4989b959a6bce3df842ff
박유빈 <pyv073@gmail.com>
2025-12-01 04:16:22 +0900

    code simplyfied by using reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer } from "react";
 import Header from "./components/Header";
 import PlantTodo from "./components/PlantTodo";
 import List from "./components/List";
 import Garden from "./components/Garden";
 import "./App.css";
 
+function reducer(todos, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...todos];
+    case "UPDATE":
+      return todos.map((v) =>
+        v.id === action.targetId ? { ...v, isDone: !v.isDone } : v
+      );
+    case "DELETE":
+      return todos.filter((v) => v.id != action.targetId);
+  }
+}
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(reducer, []);
   const [gardenTodos, setGardenTodos] = useState([]);
   const idRef = useRef("1");
   function handleTodo(content, plantType) {
-    let value = {
-      id: idRef.current++,
-      isDone: false,
-      content: `${content}`,
-      date: new Date().getTime(),
-      plantType: `${plantType}`,
-    };
-
-    setTodos([value, ...todos]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: `${content}`,
+        date: new Date().getTime(),
+        plantType: `${plantType}`,
+      },
+    });
   }
+
   const handleDone = (targetId) => {
-    setTodos(
-      todos.map((v) => (v.id === targetId ? { ...v, isDone: !v.isDone } : v))
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
+
   const handleDelete = (targetId) => {
-    setTodos(todos.filter((v) => v.id != targetId));
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
   };
   const handleSendToGarden = (targetId) => {
     setGardenTodos(
